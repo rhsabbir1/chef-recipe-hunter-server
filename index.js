1f0c7661fda9abee5de2cd405432cba1e4803fde
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-03 20:33:10 +0600

    load json data port 5000

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+const chefData = require('./data/chif.json')
 
 app.get('/', (req, res) => {
-  res.send('')
+  res.send(chefData)
 })
 
 app.listen(port, () => {
