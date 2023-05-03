9699f967f006ab607657ea02dbf5c9c5fc81d3c9
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-03 21:19:22 +0600

    get one chif by id

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app = express()
 const port = process.env.PORT || 5000;
 const chefData = require('./data/chif.json')
 
-app.get('/', (req, res) => {
+app.get('/chefData', (req, res) => {
   res.send(chefData)
+})
+
+app.get('/chefData/:id', (req, res) => {
+ const id = req.params.id;
+ const seletedChif = chefData.find(n => n.id === id);
+  res.send(seletedChif)
 })
 
 app.listen(port, () => {
