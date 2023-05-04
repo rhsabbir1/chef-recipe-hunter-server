bbd658eb4d3873ad0e2bd5ef8295950ae2b4ee47
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-04 10:26:41 +0600

    use cors

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 const chefData = require('./data/chif.json')
+const news = require('./data/news.json')
+const cors = require('cors')
+app.use(cors())
+
 
 app.get('/chefData', (req, res) => {
   res.send(chefData)
+})
+app.get('/news', (req, res) => {
+  res.send(news)
 })
 
 app.get('/chefData/:id', (req, res) => {
